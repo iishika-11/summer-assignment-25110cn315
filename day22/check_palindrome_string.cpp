3f58dc1ce30b86ea305fcf48121ea0c6944b5691
iishika-11 <ishikac0806@gmail.com>
2026-06-18 21:45:34 +0530

    Add files via upload

---
+++ day22/check_palindrome_string.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str, rev = "";
+    cout << "Enter a string: ";
+    cin >> str;
+
+    for(int i = str.length() - 1; i >= 0; i--) {
+        rev += str[i];
+    }
+
+    if(str == rev) {
+        cout << "Palindrome String";
+    } else {
+        cout << "Not a Palindrome String";
+    }
+
+    return 0;
+}
